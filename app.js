a3f342a288d41f4841610d36231c852b0b2f39f8
Taylor Ward <25tward@leisdstudent.ws>
2022-04-06 11:00:45 -0500

    4/6/22 p2

---
+++ app.js
@@ -0,0 +1,10 @@
+function run() {
+    document.getElementById("paragraph").innerHTML = "Hello World!";
+    document.getElementById("paragraph").style.backgroundColor = "lightgreen";
+    document.getElementById("paragraph").style.color = "green"
+    document.getElementById("paragraph").style.padding = "20px"
+}
+
+function randomNum() {
+    let ran = Math.floor(Math.random() * 10)
+}
